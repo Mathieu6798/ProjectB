a0eac38255c1e3d5dc2eba2187d4ae69ffd4ea3d
Mathieu <1053249@hr.nl>
2023-05-24 12:41:11 +0200

    Veranderingen voor tests

--- Logic/BarReservationlogic.cs
+++ Logic/BarReservationlogic.cs
@@ -7,6 +7,33 @@ public class BarReservationLogic : ReservationLogic
     {
         _barreservations = BarReservationAccess.LoadAll();
     }
+    public BarReservationModel GetById(int id)
+    {
+        return _barreservations.Find(i => i.ID == id);
+    }
+    public void UpdateShowReservation(BarReservationModel acc)
+    {
+        //Find if there is already an model with the same id
+        int index = _barreservations.FindIndex(s => s.ID == acc.ID);
+
+        if (index != -1)
+        {
+            //update existing model
+            _barreservations[index] = acc;
+        }
+        else
+        {
+            //add new model
+            _barreservations.Add(acc);
+        }
+        ReservationAccess.WriteAll(_reservations);
+    }
+    public void RemoveBarReservation(int barId)
+    {
+        BarReservationModel barreservation = _barreservations.FirstOrDefault(x => x.ID == barId);
+        _barreservations.Remove(barreservation);
+        BarReservationAccess.WriteAll(_barreservations);
+    }
     public bool AddBarReservations()
     {
         ReservationModel lastReservation = (_reservations).Last();
@@ -18,8 +45,23 @@ public class BarReservationLogic : ReservationLogic
         }
         if (ActualReservations + lastReservation.Chairs.Count <= MaxReservations)
         {
-            _barreservations.Add(new BarReservationModel(Menu.loggedaccount.FullName, lastReservation.Chairs.Count, show.Time));
+            var lastbar = _barreservations.LastOrDefault();
+            int newid = 0;
+            if (lastbar == null)
+            {
+                newid = 1;
+            }
+            else
+            {
+                newid = lastbar.ID + 1;
+            }
+            BarReservationModel bar = new BarReservationModel(newid, Menu.loggedaccount.FullName, lastReservation.Chairs.Count, show.Time);
+            _barreservations.Add(bar);
             BarReservationAccess.WriteAll(_barreservations);
+            _reservations.Remove(lastReservation);
+            lastReservation.BarReservationID = newid;
+            _reservations.Add(lastReservation);
+            ReservationAccess.WriteAll(_reservations);
             return true;
         }
         else
